227826741216679aa76d2f0a1cfea3f8b2dda304
Colin Wilson <github@colinwilson.dev>
2023-05-25 00:28:30 -0400

    refactor: :recycle: Refactor and fix bot.py types

--- bot.py
+++ bot.py
@@ -1,11 +1,3 @@
-"""
-Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
-
-Version: 5.5.0
-"""
-
 import asyncio
 import json
 import logging
@@ -21,77 +13,33 @@ from discord.ext.commands import Bot, Context
 
 import exceptions
 
-if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
+if not os.path.isfile(path=f"{os.path.realpath(path=os.path.dirname(__file__))}/config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
 else:
-    with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
+    with open(file=f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
         config = json.load(file)
 
-"""	
-Setup bot intents (events restrictions)
-For more information about intents, please go to the following websites:
-https://discordpy.readthedocs.io/en/latest/intents.html
-https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents
+class DadBot(commands.Bot):
+    def __init__(self, loggingFormatter, botConfig) -> None:
+        super().__init__(command_prefix=commands.when_mentioned_or(botConfig["prefix"]), intents=intents, help_command=None)
+        self.logger: logging.Logger = loggingFormatter
+        self.config: dict = botConfig
 
-
-Default Intents:
-intents.bans = True
-intents.dm_messages = True
-intents.dm_reactions = True
-intents.dm_typing = True
-intents.emojis = True
-intents.emojis_and_stickers = True
-intents.guild_messages = True
-intents.guild_reactions = True
-intents.guild_scheduled_events = True
-intents.guild_typing = True
-intents.guilds = True
-intents.integrations = True
-intents.invites = True
-intents.messages = True # `message_content` is required to get the content of the messages
-intents.reactions = True
-intents.typing = True
-intents.voice_states = True
-intents.webhooks = True
-
-Privileged Intents (Needs to be enabled on developer portal of Discord), please use them only if you need them:
-intents.members = True
+intents: discord.Intents = discord.Intents.default()
 intents.message_content = True
-intents.presences = True
-"""
-
-intents = discord.Intents.default()
-
-"""
-Uncomment this if you want to use prefix (normal) commands.
-It is recommended to use slash commands and therefore not use prefix commands.
-
-If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
-"""
-intents.message_content = True
-
-bot = Bot(
-    command_prefix=commands.when_mentioned_or(config["prefix"]),
-    intents=intents,
-    help_command=None,
-)
-
-# Setup both of the loggers
-
 
 class LoggingFormatter(logging.Formatter):
-    # Colors
-    black = "\x1b[30m"
-    red = "\x1b[31m"
-    green = "\x1b[32m"
-    yellow = "\x1b[33m"
-    blue = "\x1b[34m"
-    gray = "\x1b[38m"
-    # Styles
-    reset = "\x1b[0m"
-    bold = "\x1b[1m"
+    black: str = "\x1b[30m"
+    red: str = "\x1b[31m"
+    green: str = "\x1b[32m"
+    yellow: str = "\x1b[33m"
+    blue: str = "\x1b[34m"
+    gray: str = "\x1b[38m"
 
-    COLORS = {
+    reset: str = "\x1b[0m"
+    bold: str = "\x1b[1m"
+
+    COLORS: dict[int, str] = {
         logging.DEBUG: gray + bold,
         logging.INFO: blue + bold,
         logging.WARNING: yellow + bold,
@@ -99,62 +47,49 @@ class LoggingFormatter(logging.Formatter):
         logging.CRITICAL: red + bold,
     }
 
-    def format(self, record):
-        log_color = self.COLORS[record.levelno]
+    def format(self, record) -> str:
+        log_color: str = self.COLORS[record.levelno]
         format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
-        format = format.replace("(black)", self.black + self.bold)
+        format: str = format.replace("(black)", self.black + self.bold)
         format = format.replace("(reset)", self.reset)
         format = format.replace("(levelcolor)", log_color)
         format = format.replace("(green)", self.green + self.bold)
         formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
         return formatter.format(record)
 
+logger: logging.Logger = logging.getLogger(name="discord_bot")
+logger.setLevel(level=logging.INFO)
 
-logger = logging.getLogger("discord_bot")
-logger.setLevel(logging.INFO)
-
-# Console handler
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(LoggingFormatter())
-# File handler
+
 file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 file_handler_formatter = logging.Formatter(
     "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
 )
 file_handler.setFormatter(file_handler_formatter)
 
-# Add the handlers
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
-bot.logger = logger
 
 
-async def init_db():
+async def init_db() -> None:
     async with aiosqlite.connect(
-        f"{os.path.realpath(os.path.dirname(__file__))}/database/database.db"
+        database=f"{os.path.realpath(os.path.dirname(__file__))}/database/database.db"
     ) as db:
         with open(
-            f"{os.path.realpath(os.path.dirname(__file__))}/database/schema.sql"
+            file=f"{os.path.realpath(os.path.dirname(__file__))}/database/schema.sql"
         ) as file:
             await db.executescript(file.read())
         await db.commit()
 
-
-"""
-Create a bot variable to access the config file in cogs so that you don't need to import it every time.
-
-The config is available using the following code:
-- bot.config # In this file
-- self.bot.config # In cogs
-"""
-bot.config = config
-
+bot = DadBot(loggingFormatter=logger, botConfig=config)
 
 @bot.event
 async def on_ready() -> None:
-    """
-    The code in this event is executed when the bot is ready.
-    """
+    if bot.user is None:
+        sys.exit("Bot has no user!")
+    
     bot.logger.info(f"Logged in as {bot.user.name}")
     bot.logger.info(f"discord.py API version: {discord.__version__}")
     bot.logger.info(f"Python version: {platform.python_version()}")
@@ -171,17 +106,12 @@ async def status_task() -> None:
     """
     Setup the game status task of the bot.
     """
-    statuses = ["with your mom"]
-    await bot.change_presence(activity=discord.Game(random.choice(statuses)))
+    statuses: list[str] = ["with your mom"]
+    await bot.change_presence(activity=discord.Game(name=random.choice(seq=statuses)))
 
 
 @bot.event
 async def on_message(message: discord.Message) -> None:
-    """
-    The code in this event is executed every time someone sends a message, with or without the prefix
-
-    :param message: The message that was sent.
-    """
     if message.author == bot.user or message.author.bot:
         return
     await bot.process_commands(message)
@@ -189,13 +119,11 @@ async def on_message(message: discord.Message) -> None:
 
 @bot.event
 async def on_command_completion(context: Context) -> None:
-    """
-    The code in this event is executed every time a normal command has been *successfully* executed.
-
-    :param context: The context of the command that has been executed.
-    """
-    full_command_name = context.command.qualified_name
-    split = full_command_name.split(" ")
+    if context.command is None:
+        return
+    
+    full_command_name: str = context.command.qualified_name
+    split: list[str] = full_command_name.split(" ")
     executed_command = str(split[0])
     if context.guild is not None:
         bot.logger.info(
@@ -209,12 +137,6 @@ async def on_command_completion(context: Context) -> None:
 
 @bot.event
 async def on_command_error(context: Context, error) -> None:
-    """
-    The code in this event is executed every time a normal valid command catches an error.
-
-    :param context: The context of the normal command that failed executing.
-    :param error: The error that has been faced.
-    """
     if isinstance(error, commands.CommandOnCooldown):
         minutes, seconds = divmod(error.retry_after, 60)
         hours, minutes = divmod(minutes, 60)
@@ -225,10 +147,6 @@ async def on_command_error(context: Context, error) -> None:
         )
         await context.send(embed=embed)
     elif isinstance(error, exceptions.UserBlacklisted):
-        """
-        The code here will only execute if the error is an instance of 'UserBlacklisted', which can occur when using
-        the @checks.not_blacklisted() check in your command, or you can raise the error by yourself.
-        """
         embed = discord.Embed(
             description="You are blacklisted from using the bot!", color=0xE02B2B
         )
@@ -242,9 +160,6 @@ async def on_command_error(context: Context, error) -> None:
                 f"{context.author} (ID: {context.author.id}) tried to execute a command in the bot's DMs, but the user is blacklisted from using the bot."
             )
     elif isinstance(error, exceptions.UserNotOwner):
-        """
-        Same as above, just for the @checks.is_owner() check.
-        """
         embed = discord.Embed(
             description="You are not the owner of the bot!", color=0xE02B2B
         )
@@ -255,7 +170,7 @@ async def on_command_error(context: Context, error) -> None:
             )
         else:
             bot.logger.warning(
-                f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the bot's DMs, but the user is not an owner of the bot."
+                msg=f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the bot's DMs, but the user is not an owner of the bot."
             )
     elif isinstance(error, commands.MissingPermissions):
         embed = discord.Embed(
@@ -286,9 +201,6 @@ async def on_command_error(context: Context, error) -> None:
 
 
 async def load_cogs() -> None:
-    """
-    The code in this function is executed whenever the bot will start.
-    """
     for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
         if file.endswith(".py"):
             extension = file[:-3]
@@ -302,4 +214,4 @@ async def load_cogs() -> None:
 
 asyncio.run(init_db())
 asyncio.run(load_cogs())
-bot.run(config["token"])
+bot.run(token=config["token"])
